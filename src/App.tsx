bbc7ef6cbc94fb29425eabd065c7945d3dc1e106
Pedro Henrique <pedrodruviaro@gmail.com>
2025-04-08 14:53:41 -0300

    feat: eslint and prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
+    <div className="flex gap-2 flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
     </div>
   )
