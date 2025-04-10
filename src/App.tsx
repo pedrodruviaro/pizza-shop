86ceb840f6b3aa72940df940373cecfd0bd8b673
Pedro Henrique <pedrodruviaro@gmail.com>
2025-04-10 08:57:25 -0300

    feat: add axions and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
@@ -10,7 +12,9 @@ export function App() {
       <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
