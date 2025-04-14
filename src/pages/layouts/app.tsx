173987fe85d315d677ad7a61f7623e06f2471150
Pedro Henrique <pedrodruviaro@gmail.com>
2025-04-14 13:51:43 -0300

    feat: automatic logout

--- src/pages/layouts/app.tsx
+++ src/pages/layouts/app.tsx
@@ -1,7 +1,36 @@
 import { Header } from '@/components/header'
-import { Outlet } from 'react-router-dom'
+import { api } from '@/lib/axios'
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/sign-in', { replace: true })
+          } else {
+            // react-query not runs onSuccess fn
+            throw error
+          }
+        }
+      },
+    )
+
+    // cleanup
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
   return (
     <div className="flex flex-col min-h-screen antialiased">
       <Header />
