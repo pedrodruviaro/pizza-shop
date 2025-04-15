5c2b2c2495cfa2fb10de03ff1492c2d09e18fa04
Pedro Henrique <pedrodruviaro@gmail.com>
2025-04-14 21:23:25 -0300

    feat: add orders loading states

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -7,6 +7,7 @@ import { useQuery } from '@tanstack/react-query'
 import { getOrders } from '@/api/get-orders'
 import { useSearchParams } from 'react-router-dom'
 import { z } from 'zod'
+import { OrderTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
   const [searchParams, setSearchParmas] = useSearchParams()
@@ -21,7 +22,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     // calls queryFn every time 'pageIndex' changes (not remake the fetch if the previous page is loaded. Uses from cache)
     // queryKey: ['orders', pageIndex],
 
@@ -62,6 +63,8 @@ export function Orders() {
               </TableHeader>
 
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
+
                 {result && result.orders.map((order) => <OrderTableRow key={order.orderId} order={order} />)}
               </TableBody>
             </Table>
